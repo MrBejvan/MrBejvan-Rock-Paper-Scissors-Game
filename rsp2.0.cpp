c4a6a82943f9383e87df18f75a76155c3f0ec3dc
MrBejvan <122756021+MrBejvan@users.noreply.github.com>
2024-01-14 11:43:32 -0700

    file update; push experiment

--- rsp2.0.cpp
+++ rsp2.0.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 using namespace std;
 
-//Global variables//
-int game_counter = 0;
-int player_score = 0;
-int machine_score = 0;
-
 //This function represents the menu//
 void start_menu()
 {
@@ -41,33 +36,32 @@ int machine_selection()
 {
     int machine_choice;
     machine_choice = rand() % 3 + 1;
-    cout << "The machine has selected " << machine_choice << ". ";
+    cout << "The machine has selected " << machine_choice << ". " << endl;
     return machine_choice;
 }
 
 //This function determines the victor//
 int determine_victor(int player, int machine)
 {
-    int p_score;
-    int m_score;
+    int score_update;
     if (player != machine)
     {
         if ((player == 1 && machine == 2) || (player == 2 && machine == 3) || (player == 3 && machine == 1))
         {
             cout << "Machine scores 1 point. " << endl;
-            m_score++;
+            score_update = 0;
         }
         else
         {
             cout << "You win! Player scores 1 point. " << endl;
-            p_score++;
+            score_update = 1;
         }
     }
     else
     {
         cout << "Tie game! " << endl;
     }
-    return(p_score, m_score);
+    return(score_update);
 }
 
 //This function operates as a play again feature//
@@ -99,6 +93,10 @@ int play_again()
 
 int main()
 {
+    int game_counter = 0;
+    int player_score = 0;
+    int machine_score = 0;
+
     while (true)
     {
         start_menu();
@@ -113,13 +111,13 @@ int main()
         else
         {
             int machine_choice = machine_selection();
-            determine_victor(player_choice, machine_choice);
-            if (determine_victor(player_choice, machine_choice) == 0, 1)
+            //determine_victor(player_choice, machine_choice);
+            if (determine_victor(player_choice, machine_choice) == 0)
             {
                 machine_score++;
                 
             }
-            else if(determine_victor(player_choice, machine_choice) == 1, 0)
+            else if(determine_victor(player_choice, machine_choice) == 1)
             {
                 player_score++;
                 
